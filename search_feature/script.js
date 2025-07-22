e11134a35bcfe933b1b4100b58d718475348272f
jaspritnaveen <jaspritnaveen@gmail.com>
2025-07-22 22:27:14 +0530

    search bar updated

--- search_feature/script.js
+++ search_feature/script.js
@@ -1,19 +1,15 @@
-// Get the search input and all fruit divs
 const searchInput = document.getElementById("search");
 const fruits = document.querySelectorAll(".fruit");
 
-// Listen to input changes
 searchInput.addEventListener("input", function () {
-  const searchTerm = this.value;
-  fruits.forEach((fruit) => {
-    const fruitName = fruit.querySelector("h1").textContent.toLowerCase();
+  const searchTerm = this.value.toLowerCase();
 
-    // Show/hide fruit div based on match
-    if (fruitName.includes(searchTerm)) {
+  fruits.forEach((fruit) => {
+    const fullText = fruit.innerText.toLowerCase(); 
+    if (fullText.includes(searchTerm)) {
       fruit.style.display = "block";
     } else {
       fruit.style.display = "none";
     }
   });
 });
-
