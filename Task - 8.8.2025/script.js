03901378fc2064e3e21deb1875daddbbdc4be52c
jaspritnaveen <jaspritnaveen@gmail.com>
2025-08-11 21:07:58 +0530

    updated

--- Task - 8.8.2025/script.js
+++ Task - 8.8.2025/script.js
@@ -3,18 +3,53 @@ var formid = document.getElementById("myform");
 formid.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  var nameid = document.getElementsByName("name")[0].value;
-  var emailid = document.getElementsByName("email")[0].value;
-  var subjectid = document.getElementsByName("subject")[0].value;
-  var magtid = document.getElementById("text-box").value;
+  var nameInput = document.getElementsByName("name")[0];
+  var emailInput = document.getElementsByName("email")[0];
+  var subjectInput = document.getElementsByName("subject")[0];
+  var msgInput = document.getElementById("text-box");
+
+  var nameid = nameInput.value;
+  var emailid = emailInput.value;
+  var subjectid = subjectInput.value;
+  var magtid = msgInput.value;
+
+  document.querySelectorAll(".error").forEach((er) => (er.textContent = ""));
+
+  let valid = true;
+
+  if (nameid.trim() === "") {
+    document.getElementById("error-name").textContent = "Please fill your name";
+    nameInput.classList.add("error");
+    valid = false;
+  }
+
+  if (emailid.trim() === "") {
+    document.getElementById("error-email").textContent =
+      "Please fill your email address";
+    emailInput.classList.add("error");
+    valid = false;
+  }
+
+  if (subjectid.trim() === "") {
+    document.getElementById("error-subject").textContent =
+      "Please enter your Subject";
+    subjectInput.classList.add("error");
+    valid = false;
+  }
+
+  if (magtid.trim() === "") {
+    document.getElementById("error-mssgg").textContent =
+      "Please enter your message";
+    msgInput.classList.add("error");
+    valid = false;
+  }
 
   console.log(nameid);
   console.log(emailid);
   console.log(subjectid);
   console.log(magtid);
-  
-formid.reset();
-  
+
+  if (valid) {
+    formid.reset();
+  }
 });
-
-
